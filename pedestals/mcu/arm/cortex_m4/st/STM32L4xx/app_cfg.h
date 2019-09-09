cadeca30d6e93388d0001a392c06ede2f82182fe
mcushilei <mcushilei@users.noreply.github.com>
2019-09-09 16:53:57 +0800

    1, <NEW> add HAL driver library for STM32L4xx series from ST. Note: this library will bot be maintained by us.

---
+++ pedestals/mcu/arm/cortex_m4/st/STM32L4xx/app_cfg.h
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ *  Copyright(C)2019 by Dreistein<mcu_shilei@hotmail.com>                     *
+ *                                                                            *
+ *  This program is free software; you can redistribute it and/or modify it   *
+ *  under the terms of the GNU Lesser General Public License as published     *
+ *  by the Free Software Foundation; either version 3 of the License, or      *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  This program is distributed in the hope that it will be useful, but       *
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of                *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU          *
+ *  General Public License for more details.                                  *
+ *                                                                            *
+ *  You should have received a copy of the GNU Lesser General Public License  *
+ *  along with this program; if not, see http://www.gnu.org/licenses/.        *
+*******************************************************************************/
+
+
+//! \note do not move this pre-processor statement to other places
+#include "..\app_cfg.h"
+
+#ifndef __DRIVER_ARM_M4_STML4XX_APP_CFG_H__
+#define __DRIVER_ARM_M4_STML4XX_APP_CFG_H__
+
+/*============================ INCLUDES ======================================*/
+/*============================ MACROS ========================================*/
+#define USE_HAL_DRIVER
+
+/*============================ MACROFIED FUNCTIONS ===========================*/
+/*============================ TYPES =========================================*/
+
+#endif
+/* EOF */
