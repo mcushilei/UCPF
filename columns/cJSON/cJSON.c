d347cdbe8bc926aa1afda65dff0d9b7c16affe81
mcushilei <mcushilei@users.noreply.github.com>
2020-06-21 15:04:53 +0800

    1, <IMPROVEMENT> reduce the decimal length.

--- columns/cJSON/cJSON.c
+++ columns/cJSON/cJSON.c
@@ -503,13 +503,13 @@ static bool print_number(const cJSON * const item, printbuffer * const output_bu
     else
     {
         /* Try 15 decimal places of precision to avoid nonsignificant nonzero digits */
-        length = sprintf((char*)number_buffer, "%1.15g", d);
+        length = snprintf((char*)number_buffer, 26, "%1.4g", d);        //%1.15g
 
         /* Check whether the original double can be recovered */
         if ((sscanf((char*)number_buffer, "%lg", &test) != 1) || ((double)test != d))
         {
             /* If not, print with 17 decimal places of precision */
-            length = sprintf((char*)number_buffer, "%1.17g", d);
+            length = snprintf((char*)number_buffer, 26, "%1.6g", d);    //%1.17g
         }
     }
 
