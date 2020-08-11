3b1780bded29d26d33620484ccfd2ad7d7685098
mcushilei <mcushilei@users.noreply.github.com>
2020-08-11 15:00:13 +0800

    1, <IMPROVEMENT> rename COUNT_TRAILING_ZEROS() to U32_COUNT_TRAILING_ZEROS(). 2, <NEW> add U64_COUNT_TRAILING_ZEROS()

--- pedestals/utilities/utilities.h
+++ pedestals/utilities/utilities.h
@@ -71,18 +71,51 @@
             __V = x + 1;                        \
         } while (0)
 
-#define COUNT_TRAILING_ZEROS(__N, __V)      do {\
+#define U32_COUNT_1S(__N, __V)  do {\
             uint32_t x = __N;                   \
-            x = ((x - 1) | x) ^ x;              \
-            x = (x & 0x55555555u) + ((x >> 1)  & 0x55555555u);\
-            x = (x & 0x33333333u) + ((x >> 2)  & 0x33333333u);\
-            x = (x & 0x0F0F0F0Fu) + ((x >> 4)  & 0x0F0F0F0Fu);\
-            x = (x & 0x00FF00FFu) + ((x >> 8)  & 0x00FF00FFu);\
-            x = (x & 0x0000FFFFu) + ((x >> 16) & 0x0000FFFFu);\
+            x = (x & 0x55555555) + ((x >> 1)  & 0x55555555);\
+            x = (x & 0x33333333) + ((x >> 2)  & 0x33333333);\
+            x = (x & 0x0F0F0F0F) + ((x >> 4)  & 0x0F0F0F0F);\
+            x = (x & 0x00FF00FF) + ((x >> 8)  & 0x00FF00FF);\
+            x = (x & 0x0000FFFF) + ((x >> 16) & 0x0000FFFF);\
             __V = x;                            \
         } while (0)
 
-#define COUNT_LEADING_ZEROS(__N, __V)       do {\
+#define U64_COUNT_1S(__N, __V)  do {\
+            uint64_t x = __N;                   \
+            x = (x & 0x5555555555555555) + ((x >> 1)  & 0x5555555555555555);\
+            x = (x & 0x3333333333333333) + ((x >> 2)  & 0x3333333333333333);\
+            x = (x & 0x0F0F0F0F0F0F0F0F) + ((x >> 4)  & 0x0F0F0F0F0F0F0F0F);\
+            x = (x & 0x00FF00FF00FF00FF) + ((x >> 8)  & 0x00FF00FF00FF00FF);\
+            x = (x & 0x0000FFFF0000FFFF) + ((x >> 16) & 0x0000FFFF0000FFFF);\
+            x = (x & 0x00000000FFFFFFFF) + ((x >> 16) & 0x00000000FFFFFFFF);\
+            __V = x;                            \
+        } while (0)
+
+#define U32_COUNT_TRAILING_ZEROS(__N, __V)  do {\
+            uint32_t x = __N;                   \
+            x = ((x - 1) | x) ^ x;              \
+            x = (x & 0x55555555) + ((x >> 1)  & 0x55555555);\
+            x = (x & 0x33333333) + ((x >> 2)  & 0x33333333);\
+            x = (x & 0x0F0F0F0F) + ((x >> 4)  & 0x0F0F0F0F);\
+            x = (x & 0x00FF00FF) + ((x >> 8)  & 0x00FF00FF);\
+            x = (x & 0x0000FFFF) + ((x >> 16) & 0x0000FFFF);\
+            __V = x;                            \
+        } while (0)
+
+#define U64_COUNT_TRAILING_ZEROS(__N, __V)  do {\
+            uint64_t x = __N;                   \
+            x = ((x - 1) | x) ^ x;              \
+            x = (x & 0x5555555555555555) + ((x >> 1)  & 0x5555555555555555);\
+            x = (x & 0x3333333333333333) + ((x >> 2)  & 0x3333333333333333);\
+            x = (x & 0x0F0F0F0F0F0F0F0F) + ((x >> 4)  & 0x0F0F0F0F0F0F0F0F);\
+            x = (x & 0x00FF00FF00FF00FF) + ((x >> 8)  & 0x00FF00FF00FF00FF);\
+            x = (x & 0x0000FFFF0000FFFF) + ((x >> 16) & 0x0000FFFF0000FFFF);\
+            x = (x & 0x00000000FFFFFFFF) + ((x >> 16) & 0x00000000FFFFFFFF);\
+            __V = x;                            \
+        } while (0)
+    
+#define U32_COUNT_LEADING_ZEROS(__N, __V)   do {\
             uint32_t x = __N;                   \
             x |= x >> 1;                        \
             x |= x >> 2;                        \
